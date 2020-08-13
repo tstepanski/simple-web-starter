7fba6774dddaf40bbd5f7863ddaf8dc8bd95e935
Timothy M. Stepanski <timothy@timstepanski.com>
2020-08-13 10:14:58 -0400

    Delete Watcher.js

--- Watcher.js
+++
@@ -1,9 +0,0 @@
-import {Printer} from "./Printer";
-
-export class Watcher {
-    static watchThis(id) {
-        document.getElementById(id).onclick = () => {
-            new Printer().printHelloWord();
-        };
-    }
-}
